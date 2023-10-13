affd62fdd986168cf2e2182de0ebe9369348d677
Mr Fynn <104089609+MrFynnA@users.noreply.github.com>
2023-10-13 20:18:59 +0000

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import SignInLinks from "./Signedinlinks";
 import SignOutLinks from "./Signedoutlinks";
 import MobileNav from "./MobileNavbar/MobileNavbar";
+import { useSelector } from "react-redux";
+
 
 
 
 const Navbar=()=>{
+    const mobileMenuActive= useSelector(state=>state.visibility.mobileMenu)
 
 
     return(
         <nav className="bg-gray-800  w-full text-white">
             <div className='flex justify-around items-center py-4 px-1'>
-            <div className="md:hidden ml-[-1rem]  relative left-1 !z-[80]"><MobileNav/></div>
+            <div className={`${mobileMenuActive && '!fixed !left-5'} md:hidden ml-[-1rem]  relative left-1 !z-[80]`}><MobileNav/></div>
           <Link to={'/'} className="text-3xl  font-bold max-md:relative max-md:right-2">StarPros</Link>
           <SignInLinks/>
           <SignOutLinks/>
